52e8da9bf718d69d43d8008afa8f2f903b1a441b
sascha <saschavv@gmail.com>
2017-03-30 00:21:34 +0200

    Some small changes to fix to make it work in my own project.

--- bootstrap-reload.js
+++ bootstrap-reload.js
@@ -1,65 +1,71 @@
 (function( $, window, document, undefined ) {
-	var Reload = function(elem, options) {
-		this.$elem = $(elem);
-		this.options = options;
-	};
+   var Reload = function(elem, options) {
+      this.$elem = $(elem);
+      this.options = options;
+   };
 
-	Reload.prototype.defaults = {
-		time: 3000,
-		autoReload: false,
-		beforeReload: function(){},
-		afterReload: function(){}
-	};
+   Reload.prototype.defaults = {
+      time: 3000,
+      autoReload: false,
+      beforeReload: function(){},
+      afterReload: function(){}
+   };
 
-	Reload.prototype.init = function() {
-		var self = this;
+   Reload.prototype.init = function() {
+      var self = this;
 
-		// Set the configuration parameters
-		self.config = $.extend({},self.defaults,self.options);
+      // Set the configuration parameters
+      self.config = $.extend({}, self.defaults, self.options);
 
-		// Set the container for refresh animation
-		self.config.refreshContainer = $(self.config.refreshContainer) || self.$elem.find('.refresh-container');
+      // Set the container for refresh animation
+      self.config.refreshContainer = $(self.$elem.find('.refresh-container'));
 
-		// Set the container to update the data with
-		self.config.dataContainer = $(self.config.dataContainer) || self.$elem.find('.data-container');
-		
-		self.$elem.find('.fa-refresh').click(self.reload());
+      // Set the container to update the data with
+      self.config.dataContainer = self.$elem.find('.refresh-data');
 
-		if(self.config.autoReload) {
-			setInterval(self.reload,self.config.time);
-		}
+      self.config.refreshButton = self.$elem.find('.fa-refresh');
 
-		return self;
-	};
+      self.config.refreshButton.click(function () {
+          self.reload()
+          return false
+      });
 
-	Reload.prototype.reload = function(){
-		var _self = this;
-		_self.$elem.find('.fa-refresh').addClass('fa-spin');
-		_self.cofig.refreshContainer.fadeIn();
+      if (self.config.autoReload) {
+          setInterval((() => self.reload()), self.config.time);
+          self.reload();
+      }
 
-		// Send the AJAX request to fetch the data
-		$.ajax({
-			url: _self.$elem.data('url'),
-			async: true,
-			beforeSend: _self.config.beforeReload,
-			success: function(data) {
-				_self.config.dataContainer.html(data);
-				_self.config.afterReload();
-			}
+      return self;
+   };
 
-		});
+   Reload.prototype.reload = function(){
+       var _self = this;
 
-	};
+      _self.config.refreshButton.addClass('fa-spin');
 
-	// Register the plugin to JQuery
-	$.fn.reload = function(options) {
-		this.each(function() {
-			var $this, reload;
-			$this = $(this);
-			reload = new Reload(this, options);
-			return reload.init();
+      //console.log("Get " + _self.$elem.data('url'));
 
-		});
-	};
+      $.ajax({
+         url: _self.$elem.data('url'),
+         async: true,
+         beforeSend: _self.config.beforeReload,
+         success: function (data) {
+             _self.config.dataContainer.html(data);
+            _self.config.afterReload();
+            _self.config.refreshButton.removeClass('fa-spin');
+         }
+      });
+   };
+
+   // Register the plugin to JQuery
+   $.fn.reload = function(options) {
+      this.each(function() {
+         var $this, reload;
+         $this = $(this);
+         reload = new Reload(this, options);
+         return reload.init();
+
+      });
+   };
 
 })( window.jQuery, window, document );
